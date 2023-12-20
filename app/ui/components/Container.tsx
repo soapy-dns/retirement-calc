d0ef58d4cd8c4aafb0975b0892b3c8cdf3d2406d
neil <neil@NEIL>
2023-12-20 21:47:57 +0000

    add padding to container

--- app/ui/components/Container.tsx
+++ app/ui/components/Container.tsx
@@ -2,5 +2,5 @@ interface IContainer {
   children: React.ReactNode
 }
 export const Container: React.FC<IContainer> = ({ children }) => {
-  return <div className=" container mx-auto max-w-2xl">{children}</div>
+  return <div className=" container mx-auto max-w-2xl px-4">{children}</div>
 }
