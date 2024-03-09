f56792a8005da3cb9883b1dfbd74a04277ef23b1
neil <neil_soutar@yahoo.com>
2024-03-09 02:50:51 +0000

    altered styling for ConfigNavBar

--- app/(withCalculation)/(withNavBar)/(withContainer)/(config)/ConfigNavBar.tsx
+++ app/(withCalculation)/(withNavBar)/(withContainer)/(config)/ConfigNavBar.tsx
@@ -23,35 +23,41 @@ export const ConfigNavBar = () => {
   const selectedTabClassNames = "border-b-4 border-primary "
 
   return (
-    <div className=" mb-4 flex  justify-around bg-primary-lightest pt-2 pb-1">
+    <div className=" mb-4 grid grid-cols-3 bg-primary-lightest pt-2 pb-1">
       <button
         onClick={handleContextClick}
-        className={`${tabColors} ${activeTab === ConfigTab.context ? selectedTabClassNames : null}`}
+        className={`${tabColors} flex justify-center gap-2 mx-2 ${
+          activeTab === ConfigTab.context ? selectedTabClassNames : null
+        }`}
       >
-        <div className="mx-4 flex gap-2">
-          <div className="hidden sm:inline">Context</div>
-          <GlobeAltIcon className="h-6 w-6" />
-        </div>
+        {/* <div className="mx-4 flex gap-2"> */}
+        <div className="hidden sm:inline">Context</div>
+        <GlobeAltIcon className="h-6 w-6" />
+        {/* </div> */}
       </button>
 
       <button
         onClick={handleAssetsClick}
-        className={`${tabColors} ${activeTab === ConfigTab.assets ? selectedTabClassNames : null}`}
+        className={`${tabColors} flex justify-center gap-2 mx-2 ${
+          activeTab === ConfigTab.assets ? selectedTabClassNames : null
+        }`}
       >
-        <div className="mx-4 flex gap-2">
-          <div className="hidden sm:inline">Assets</div>
-          <CurrencyDollarIcon className="h-6 w-6" />
-        </div>
+        {/* <div className="mx-4 flex gap-2"> */}
+        <div className="hidden sm:inline">Assets</div>
+        <CurrencyDollarIcon className="h-6 w-6" />
+        {/* </div> */}
       </button>
 
       <button
         onClick={handleTransfersClick}
-        className={`${tabColors} ${activeTab === ConfigTab.transfers ? selectedTabClassNames : null}`}
+        className={`${tabColors} flex justify-center gap-2 mx-2 ${
+          activeTab === ConfigTab.transfers ? selectedTabClassNames : null
+        }`}
       >
-        <div className="mx-4 flex gap-2">
-          <div className="hidden sm:inline">Transfers</div>
-          <ArrowPathRoundedSquareIcon className="h-6 w-6" />
-        </div>
+        {/* <div className="mx-4 flex gap-2"> */}
+        <div className="hidden sm:inline">Transfers</div>
+        <ArrowPathRoundedSquareIcon className="h-6 w-6" />
+        {/* </div> */}
       </button>
     </div>
   )
