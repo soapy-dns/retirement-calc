741937a94f30b589f7b8740c460f3455295c4474
neil <neil_soutar@yahoo.com>
2024-04-22 15:56:06 +0100

    update scottish tax rates

--- app/lib/calculations/tax/taxCalcs/UkIncomeTaxCalc.ts
+++ app/lib/calculations/tax/taxCalcs/UkIncomeTaxCalc.ts
@@ -7,19 +7,22 @@ export class UkIncomeTaxCalc extends IncomeTaxCalc {
   taxRate3 = 0.2
   taxRate4 = 0.21
   taxRate5 = 0.42
-  taxRate6 = 0.47
+  taxRate6 = 0.45
+  taxRate7 = 0.48
 
   bandTop1 = 12570
-  bandTop2 = 14732
-  bandTop3 = 25688
-  bandTop4 = 43663
-  bandTop5 = 125140
+  bandTop2 = 14876
+  bandTop3 = 26561
+  bandTop4 = 43662
+  bandTop5 = 75000
+  bandTop6 = 125140
 
   taxTo1 = 0
   taxTo2 = (this.bandTop2 - this.bandTop1) * this.taxRate2
   taxTo3 = (this.bandTop3 - this.bandTop2) * this.taxRate3 + this.taxTo2
   taxTo4 = (this.bandTop4 - this.bandTop3) * this.taxRate4 + this.taxTo3
   taxTo5 = (this.bandTop5 - this.bandTop4) * this.taxRate4 + this.taxTo4
+  taxTo6 = (this.bandTop6 - this.bandTop5) * this.taxRate5 + this.taxTo5
 
   getTax(income: number, year: number): number {
     const { incomeInTodaysMoney, inflationFactor } = getIncomeInTodaysMoney(
@@ -31,13 +34,15 @@ export class UkIncomeTaxCalc extends IncomeTaxCalc {
 
     let taxAmtInTodaysMoney
     switch (true) {
+      case incomeInTodaysMoney > this.bandTop6:
+        taxAmtInTodaysMoney = (incomeInTodaysMoney - this.bandTop6) * this.taxRate7 + this.taxTo6
+        break
       case incomeInTodaysMoney > this.bandTop5:
         taxAmtInTodaysMoney = (incomeInTodaysMoney - this.bandTop5) * this.taxRate6 + this.taxTo5
         break
       case incomeInTodaysMoney > this.bandTop4:
         taxAmtInTodaysMoney = (incomeInTodaysMoney - this.bandTop4) * this.taxRate5 + this.taxTo4
         break
-
       case incomeInTodaysMoney > this.bandTop3:
         taxAmtInTodaysMoney = (incomeInTodaysMoney - this.bandTop3) * this.taxRate4 + this.taxTo3
         break
