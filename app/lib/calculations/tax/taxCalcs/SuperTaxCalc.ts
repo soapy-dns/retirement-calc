362fdd87723f3b2fc0c99c60aa3b6d00be8cdb11
neil <neil_soutar@yahoo.com>
2024-04-23 18:16:12 +0100

    refactoring of tax configuration

--- app/lib/calculations/tax/taxCalcs/SuperTaxCalc.ts
+++ app/lib/calculations/tax/taxCalcs/SuperTaxCalc.ts
@@ -1,7 +1,7 @@
-import config from "@/app/lib/calculations/config.json"
+import config from "@/app/lib/config.json"
 import { Country } from "@/app/lib/data/schema/config"
 
 export const getSuperIncomeTax = (income: number, taxRegimeCountry: Country): number => {
-  const { superIncomeTaxRate } = config[taxRegimeCountry]
+  const { superIncomeTaxRate } = config.superTax[taxRegimeCountry]
   return income * superIncomeTaxRate
 }
