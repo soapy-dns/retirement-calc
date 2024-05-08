3c24601a1cecc1e9b4d883c9250ae8af688496c3
neil <neil_soutar@yahoo.com>
2024-05-08 10:22:34 +0100

    add validation to ensure the exchange rate is set when currency country !== tax resident country

--- app/(withCalculation)/(withNavBar)/(withContainer)/(config)/context/general/page.tsx
+++ app/(withCalculation)/(withNavBar)/(withContainer)/(config)/context/general/page.tsx
@@ -11,11 +11,20 @@ import { useForm } from "react-hook-form"
 import GeneralContextForm from "./GeneralContextForm"
 import { CountryEnum, IsFormNumberOpt } from "@/app/lib/data/schema/config/schemaUtils"
 
-const FormSchema = z.object({
-  taxResident: CountryEnum,
-  currency: CountryEnum,
-  au2ukExchangeRate: IsFormNumberOpt // TODO: only optional if taxResident !== currency
-})
+const FormSchema = z
+  .object({
+    taxResident: CountryEnum,
+    currency: CountryEnum,
+    au2ukExchangeRate: IsFormNumberOpt // TODO: only optional if taxResident !== currency
+  })
+  .refine(
+    ({ taxResident, currency, au2ukExchangeRate }) => {
+      if (taxResident !== currency && !au2ukExchangeRate) return false
+      return true
+    },
+    { message: "The exchange rate is required", path: ["au2ukExchangeRate"] }
+  )
+
 export type FormDataType = z.infer<typeof FormSchema>
 
 const GeneralContextEditPage: React.FC = () => {
