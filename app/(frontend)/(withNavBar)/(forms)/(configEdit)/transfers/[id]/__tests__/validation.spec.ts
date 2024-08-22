44ccd3690ed1d65569b8a098ae1af25c2ae2f4c0
neil <neil_soutar@yahoo.com>
2024-08-22 13:24:36 +0100

    improve validation of total transfers

--- app/(frontend)/(withNavBar)/(forms)/(configEdit)/transfers/[id]/__tests__/validation.spec.ts
+++ app/(frontend)/(withNavBar)/(forms)/(configEdit)/transfers/[id]/__tests__/validation.spec.ts
@@ -1,18 +1,18 @@
 import { Transfer } from "@/app/lib/data/schema/config"
 import { FormDataType } from "../FormSchema"
-import { testForMultipleMigrateAll } from "../validation"
+import { testForMultipleMigrateAllFrom } from "../validation"
 
 describe("testForMultipleMigrateAll", () => {
   it("should return true when add full transfer and no transfers exit", () => {
     const formData: FormDataType = { from: "A", to: "B", year: 2023, migrateAll: "Y" }
-    const result = testForMultipleMigrateAll(formData)
+    const result = testForMultipleMigrateAllFrom(formData)
     expect(result).toBe(true)
   })
 
   it("should return false when add full transfer and full transfer exits", () => {
     const formData: FormDataType = { from: "A", to: "B", year: 2023, migrateAll: "Y" }
     const transfers: Transfer[] = [{ id: "A", from: "A", to: "C", year: 2023, migrateAll: true }]
-    const result = testForMultipleMigrateAll(formData, transfers)
+    const result = testForMultipleMigrateAllFrom(formData, transfers)
     expect(result).toBe(false)
   })
 })
