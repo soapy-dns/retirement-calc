de976438d5783cb9bdc4be98ef278f925f180ab6
neil <neil_soutar@yahoo.com>
2024-12-24 14:46:13 +1100

    fix error with % of drawdown wh is taxable

--- app/lib/calculations/tax/getPercentageOfDrawdownTaxable.ts
+++ app/lib/calculations/tax/getPercentageOfDrawdownTaxable.ts
@@ -9,12 +9,13 @@ export const getPercentageOfDrawdownTaxable = (
   assetCountry: Country = "AU",
   assetClass: AssetGroup
 ) => {
-  if (assetClass === AssetGroup.super && assetCountry === "AU" && taxResident === "AU") {
-    return 0
-  }
+  // if (assetClass === AssetGroup.super && assetCountry === "AU" && taxResident === "AU") {
+  //   return 0
+  // }
+  // it doesn't matter if au or uk pension, if you are resident in UK only 25% is tax free.
   if (assetClass === AssetGroup.super && ukCountrys.includes(taxResident)) {
     return 75
   }
 
-  return 100
+  return 0
 }
