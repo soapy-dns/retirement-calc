0bdb8ed3175ef6f9d13d6ec86ed431c99ac82e6f
neil <neil_soutar@yahoo.com>
2025-03-08 16:57:59 +1100

    minor refactor to LivingExpensesPage.spec.tsx

--- app/(frontend)/(withNavBar)/(forms)/(configEdit)/context/livingexpenses/__tests__/LivingExpensesPage.spec.tsx
+++ app/(frontend)/(withNavBar)/(forms)/(configEdit)/context/livingexpenses/__tests__/LivingExpensesPage.spec.tsx
@@ -3,12 +3,8 @@ import { screen } from "@testing-library/dom"
 import { act, render, waitFor } from "@testing-library/react"
 
 import LivingExpensesPage from "../page"
-import { HelpModalContext } from "@/app/ui/context/HelpModalProvider"
 import { generateMock } from "@anatine/zod-mock"
 import { ScenarioSchema } from "@/app/lib/data/schema/config"
-import { getDefaultScenarios } from "@/app/lib/data/scenarios"
-import { ScenarioContext } from "@/app/ui/context/scenario/ScenarioContext"
-import { forEach } from "lodash"
 import { ProviderWrapper } from "@/app/ui/testing/ProviderWrappers"
 
 const goBack = jest.fn()
@@ -60,7 +56,7 @@ describe("Living expenses edit page", () => {
       const years = screen.getAllByPlaceholderText("Add a year")
       expect(years).toHaveLength(3)
       // third year is the modal...  There must be a better way to do this
-      forEach(years, (year, index) => {
+      years.forEach((year, index) => {
         const val = ["2024", "2038", ""][index]
         expect(year).toHaveAttribute("value", val)
       })
@@ -68,7 +64,7 @@ describe("Living expenses edit page", () => {
       // check amounts entered properly
       const amounts = screen.getAllByPlaceholderText("Add an amount")
       expect(amounts).toHaveLength(3)
-      forEach(amounts, (amount, index) => {
+      amounts.forEach((amount, index) => {
         // third values is the modal...  There must be a better way to do this
         const val = ["80001", "50000", ""][index]
         expect(amount).toHaveAttribute("value", val)
